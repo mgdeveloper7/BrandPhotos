a3fd5b200d36a74051e2cd20d0947be6cdf8345a
Mark Gumbs <mgdeveloper7@gmail.com>
2017-09-12 09:03:57 +0100

    Initial Commit

---
+++ BrandPhotos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BrandPhotos
+//
+//  Created by Mark Gumbs on 12/09/2017.
+//  Copyright © 2017 Mark Gumbs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
